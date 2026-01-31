48fa05ce6e2c8c5172ddd1f8c4f64dc9076f47af
aperez0103 <angel13per@gmail.com>
2026-01-30 19:22:37 -0500

    Problem Set 3 Submission

---
+++ 03_week/outputs/writeup.md
@@ -0,0 +1,39 @@
+---
+output:
+  html_document: default
+---
+
+### getwd()
+
+"/Users/angel/Desktop/PSU/2025-26/Spring '26/PLSC 498/Problem Sets/plsc_498/03_week"
+
+### list.files("data")
+
+[1] "codebook.md"\
+[2] "dataverse_files.zip"\
+[3] "HOUSE_precinct_general.csv" [4] "README.md"
+
+### list.files()
+
+[1] "03_week.Rproj" "data" "figures"\
+[4] "outputs" "problem_set" "scripts"\
+[7] "slides"
+
+### nrow(county_df)
+
+[1] 1698\
+The total number of unique counties in the aggregated dataset is 1698.
+
+### summary(county_df\$county_total_votes)
+
+The lowest amount of votes a county recorded was 24, while the highest number of votes recieved is 875,210. The dataset has a median vote count of 6,486.
+
+### summary(county_df\$rep_share)
+
+The bounds of the Republican share of votes by county are 0 and 1, as the share is presented as a percentage of total votes. The average republican share of votes across all counties is 60.55%, with 50% of the data falling between 47.49% and 76.26%
+
+### Interpretation and Explanation
+
+In both plots, the the Republican share of votes by counties is plotted on the y-axis. Along the x-axis, in [Figure 1](../figures/plot_raw.png), is the unscaled, raw total county vote totals, while [Figure 2](../figures/plot_scaled.png) uses a log scale of the same data. The color is mapped on a continuous scale according to the share of votes held by republicans, showing how republican vote shares are, with values less than 50% being more democrat dominant. In the unscaled plot, it is more difficult to see county total votes on a case-by-case basis as it relates to the republican share of those votes, it is also difficult to interpret what each point is conveying (that is, it is difficult to tell what vote counts are more frequent under 250,000 votes). Both of these issues are remedied in the scaled plot, with a more spread out plot of the data being visualized, allowing for white space to exists and differentiate points from on another. The scaled plot does not present any new issues, nor does it make it any more difficult in interpreting its contents; however, if the outlier effects were meant to be visualized in this scatterplot, then it becomes very difficult to intuitively analyze it with the logged scale in the x-axis. In presenting the results to a general audience, the scaled plot ([Figure 2](../figures/plot_scaled.png)) would be the figure I present. It is uncluttered, clear, and visually appealing compared to the unscaled version ([Figure 1](../figures/plot_raw.png)).
+
+
---
+++ 03_week/scripts/lab.R
@@ -0,0 +1,84 @@
+## In-Class assignment script, creates properly scaled figure for overdispered Election Data
+
+###setup----
+library(dplyr)
+library(scales)
+library(ggplot2)
+
+set.seed(123)
+
+###Data----
+df <- read.csv("data/HOUSE_precinct_general.csv")
+
+head(df) #inspect data
+names(df) #inspect data
+
+##filter data 
+df_keep <- df %>%
+  filter(stage == "GEN") %>%
+  filter(party_simplified == c("DEMOCRAT", "REPUBLICAN")) %>%
+  filter(county_fips!="") #removes empty county info using county name
+
+##county-level aggregation
+county_df <- df_keep %>% 
+  group_by(county_name) %>%
+  summarize(
+    dem_votes = sum(votes[party_simplified == "DEMOCRAT"], na.rm = TRUE),
+    rep_votes = sum(votes[party_simplified == "REPUBLICAN"], na.rm = TRUE),
+    .groups = "drop"
+  ) %>%
+  mutate(
+    county_total_votes = dem_votes + rep_votes, 
+    rep_share = rep_votes / (dem_votes + rep_votes)
+  ) %>%
+  na.omit(county_df$rep_share) #drops any NaN values caused by 0 votes
+
+##checks
+#nrow(county_df)                 # number of unique counties
+#summary(county_df$dem_votes)    #summary by dem_votes     
+#summary(county_df$rep_votes)    #summary by rep_votes
+#range(county_df$rep_share)      #range by rep_share, should be between 0 & 1
+
+###Plots----
+##unscaled plot 
+plot_raw <- ggplot(county_df, aes(county_total_votes, rep_share, color = rep_share)) +
+  geom_point(size = 3) +
+  scale_color_continuous(palette = c("blue", "grey", "red")) +
+  theme_bw() + 
+  ylab("Republican Share of Votes") + 
+  xlab("Total County Votes") +
+  scale_y_continuous(labels = scales::label_percent()) + #display y-axis scale as percentages
+  scale_x_continuous(labels = scales::comma) + #display x-axis scale using commas
+  ggtitle("Figure 1: Republican Share of County Votes") + 
+  theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) #centers title with plot
+
+ggsave("figures/plot_raw.png", plot = plot_raw, scale = 1.5) #scale for readability 
+
+##scaled plot (log)
+plot_scaled <- ggplot(county_df, aes(county_total_votes, rep_share, color = rep_share)) +
+  geom_point(size = 3) +
+  scale_color_continuous(palette = c("blue", "grey", "red")) +
+  theme_bw() + 
+  ylab("Republican Share of Votes") + 
+  xlab("Total County Votes\n(log10 Scaled)") +
+  scale_y_continuous(labels = scales::label_percent()) + #display y-axis scale as percentages
+  scale_x_log10(labels = scales::comma) + #log scaled x-axis using commas
+  ggtitle("Figure 2: Republican Share of County Votes (Scaled)") + 
+  theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) #centers title with plot
+
+ggsave("figures/plot_scaled.png", plot = plot_scaled, scale = 1.5) #scale for readability 
+
+
+##optional challenge
+plot_scaled_extra <- ggplot(county_df, aes(log1p(county_total_votes), rep_share, color = rep_share)) +
+  geom_point(size = 3) +
+  scale_color_continuous(palette = c("blue", "grey", "red")) +
+  theme_bw() + 
+  ylab("Republican Share of Votes") + 
+  xlab("Total County Votes\n(Log Scaled)") +
+  scale_y_continuous(labels = scales::label_percent()) + #display y-axis scale as percentages
+  scale_x_continuous(labels = scales::comma) + #log scaled x-axis using commas
+  ggtitle("Figure 2: Republican Share of County Votes (Scaled)") + 
+  theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) #centers title with plot
+
+ggsave("figures/plot_scaled_extra.png", plot = plot_scaled_extra, scale = 1.5) #scale for readability 
