7016104addec20f09db256c835afbe751a3bfa09
aperez0103 <angel13per@gmail.com>
2026-02-05 11:51:37 -0500

    test commit

---
+++ 04_week/outputs/writeup.md
@@ -0,0 +1,21 @@
+## getwd()  
+[1] "/Users/angel/Desktop/PSU/2025-26/Spring '26/PLSC 498/Problem Sets/plsc_498/04_week"
+
+## list.files()  
+[1] "04_week.Rproj" "data"          "figures"      
+[4] "outputs"       "problem_set"   "scripts"      
+[7] "slides"  
+
+## nrow(df)  
+[1] 10125
+
+## ncol(df)  
+[1] 22
+
+## table(df4$congress)  
+101 106 111 116 
+102 102 111 102 
+
+## table(df4$chamber)  
+Senate 
+   417 
---
+++ 04_week/scripts/lab4.R
@@ -0,0 +1,101 @@
+# ----------------------------------
+# Script name: lab4.R
+# Purpose: PLSC 498 Lab 4 - Senate Ideology
+# ----------------------------------
+
+##setup----
+library(ggplot2)
+library(dplyr)
+set.seed(123)
+
+##data----
+df <- read.csv("data/Sall_members.csv")
+
+#check data
+dim(df)
+names(df)
+
+summary(df$nominate_dim1)
+summary(df$nominate_dim2)
+
+#isolate relevant data 
+ df4 <- df %>% filter(chamber == "Senate") %>% 
+   filter(congress %in% c(106, 106, 111, 116))
+
+#check  
+table(df4$congress)
+table(df4$chamber)
+
+##plots----
+
+#Congress 101 Senate plot
+congress_id <- 101
+
+p <- ggplot(df4 %>% filter(congress == congress_id),
+            aes(nominate_dim1, nominate_dim2, color = nominate_dim1)) +
+  geom_point() +
+  labs(title = "Senate ideology:101st Congress", x = "dim1", y = "dim2") +
+theme_minimal() +
+  theme(plot.background = element_rect(fill = "white", color = NA)) +
+  scale_color_gradient2(
+    low = "blue",
+    mid = "grey",
+    high = "red",
+    midpoint = 0
+  )
+
+ggsave("figures/senate_101.png", p, width = 8, height = 5)
+
+#Congress 106 Senate plot
+congress_id <- 106
+
+p <- ggplot(df4 %>% filter(congress == congress_id),
+            aes(nominate_dim1, nominate_dim2, color = nominate_dim1)) +
+  geom_point() +
+  labs(title = "Senate ideology:106th Congress", x = "dim1", y = "dim2") +
+  theme_minimal() +
+  theme(plot.background = element_rect(fill = "white", color = NA)) +
+  scale_color_gradient2(
+    low = "blue",
+    mid = "grey",
+    high = "red",
+    midpoint = 0
+  )
+
+ggsave("figures/senate_106.png", p, width = 8, height = 5)
+
+#Congress 111 Senate plot
+congress_id <- 111
+
+p <- ggplot(df4 %>% filter(congress == congress_id),
+            aes(nominate_dim1, nominate_dim2, color = nominate_dim1)) +
+  geom_point() +
+  labs(title = "Senate ideology:111th Congress", x = "dim1", y = "dim2") +
+  theme_minimal() +
+  theme(plot.background = element_rect(fill = "white", color = NA)) +
+  scale_color_gradient2(
+    low = "blue",
+    mid = "grey",
+    high = "red",
+    midpoint = 0
+  )
+
+ggsave("figures/senate_111.png", p, width = 8, height = 5)
+
+#Congress 116 Senate plot
+congress_id <- 116
+
+p <- ggplot(df4 %>% filter(congress == congress_id),
+            aes(nominate_dim1, nominate_dim2, color = nominate_dim1)) +
+  geom_point() +
+  labs(title = "Senate ideology:116th Congress", x = "dim1", y = "dim2") +
+  theme_minimal() +
+  theme(plot.background = element_rect(fill = "white", color = NA)) +
+  scale_color_gradient2(
+    low = "blue",
+    mid = "grey",
+    high = "red",
+    midpoint = 0
+  )
+
+ggsave("figures/senate_116.png", p, width = 8, height = 5)
