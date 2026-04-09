0b930996584a4e1c4a3aa33cc41ec57d70165326
jfedgerton <jared.edgerton@gmail.com>
2026-04-09 09:33:19 -0400

    Added the new problem set

--- 15_week/data/_tmp_inspect.R
+++ 15_week/data/_tmp_inspect.R
@@ -1,20 +1,20 @@
-files <- c(
-  "04_week/data/Sall_members.csv",
-  "09_week/data/state_df.rds",
-  "12_week/data/presidential.rds",
-  "13_week/data/state_data.rds",
-  "13_week/data/us_arrests.rds",
-  "07_week/data/battle_deaths.rds",
-  "02_week/data/vdem.rds",
-  "14_week/data/survey_democracy.rds"
-)
-for (p in files) {
-  cat("===", p, "===\n")
-  tryCatch({
-    d <- if (grepl("csv$", p)) read.csv(p) else readRDS(p)
-    cat("dim:", nrow(d), "x", ncol(d), "\n")
-    cat("cols:", paste(names(d), collapse = ", "), "\n")
-    print(utils::head(d, 2))
-    cat("\n")
-  }, error = function(e) cat("err:", conditionMessage(e), "\n"))
+setwd("15_week/data")
+cat("=== isis_count_data.rds ===\n")
+isis <- readRDS("isis_count_data.rds")
+cat("class:", class(isis)[1], "dim:", dim(isis)[1], "x", dim(isis)[2], "\n")
+cat("cols:", paste(names(isis), collapse = ", "), "\n")
+print(utils::head(isis, 3))
+
+cat("\n=== data_for_analysis.Rda ===\n")
+env <- new.env()
+load("data_for_analysis.Rda", envir = env)
+cat("objects:", paste(ls(env), collapse = ", "), "\n")
+for (nm in ls(env)) {
+  obj <- env[[nm]]
+  if (is.data.frame(obj)) {
+    cat("--", nm, "dim:", nrow(obj), "x", ncol(obj), "\n")
+    cat("   cols:", paste(names(obj), collapse = ", "), "\n")
+  } else {
+    cat("--", nm, "class:", class(obj)[1], "\n")
+  }
 }
